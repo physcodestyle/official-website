cc828e7f7db81665d2cc0ec498fc1a58275d550c
Igor Korovchenko <korovchenko@phys.vsu.ru>
2021-01-18 11:46:51 +0000

    Испраляет скрол и иконку

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -7,13 +7,23 @@
   const isRussianPage = !url.includes('/en') ? false : true;
   if (isMenuButtonMode) {
     const menuTitle = document.querySelector('.button__title');
+    const menuIcon = document.querySelector('.header__menu-button .button__icon');
     menuButton.addEventListener('click', () => {
       const menu = document.querySelector('.header__navigation');
+      const page = document.querySelector('.page');
       menu.classList.toggle('header__navigation--open');
       if (menu.classList.contains('header__navigation--open')) {
-        menuTitle.innerHTML = isRussianPage ? 'Назад' : 'Back';
+        menuTitle.innerHTML = isRussianPage ? 'Закрыть' : 'Close';
+        menuIcon.setAttribute('src', '/assets/Icon/Close.svg');
+        menuIcon.setAttribute('width', '24');
+        menuIcon.setAttribute('height', '24');
+        page.setAttribute('style', 'overflow-y: hidden;');
       } else {
         menuTitle.innerHTML = isRussianPage ? 'Меню' : 'Menu';
+        menuIcon.setAttribute('src', '/assets/Logo/logo.svg');
+        menuIcon.setAttribute('width', '32');
+        menuIcon.setAttribute('height', '32');
+        page.setAttribute('style', '');
       }
     });
   } else {
